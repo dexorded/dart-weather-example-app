0c9fee54ed1b2c101a165c9bfc72a88a78e28986
dexorded <david.black2@mail.ru>
2023-07-24 18:53:50 +0300

    fix: move dio to http

--- lib/src/data/datasources/open_map_datasource.dart
+++ lib/src/data/datasources/open_map_datasource.dart
@@ -1,20 +1,32 @@
-import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+// import 'package:dio/dio.dart';
 import 'package:template_project/src/data/models/weather_model.dart';
 
 class OpenMapDatasource {
   static const serverPath = 'https://api.openweathermap.org/data/3.0/onecall';
+  static const key = 'f0a05a07b491b12e551d043238a8cf73';
 
-  final Dio client = Dio();
+  // final Dio client = Dio();
 
   Future<WeatherModel> postWeatherRequest({
     required String lat,
     required String lon,
   }) async {
-    final Response response = await client.get(serverPath, data: {
-      'lat': lat,
-      'lon': lon,
-    });
+    var url = Uri.https(
+      'api.openweathermap.org',
+      'data/2.8/onecall',
+      {
+        'appid': key,
+        'lat': lat,
+        'lon': lon,
+      },
+    );
 
-    return WeatherModel.fromJson(response.data);
+    final http.Response response = await http.get(url);
+
+    // print('Response status: ${response.statusCode}');
+    // print('Response body: ${response.body}');
+
+    return WeatherModel.fromJson(response.body);
   }
 }
